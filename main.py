37f6ea090e9f5b0149feef5a1fa494525c08a003
fluffypancakes159 <33842890+fluffypancakes159@users.noreply.github.com>
2023-02-27 19:01:05 -0600

    added functionality to swap between work mode and break mode, still doesn't actually do anything when the timer hits 0, the text just switches to allow the user to start again whenever

--- main.py
+++ main.py
@@ -4,18 +4,37 @@ import time
 
 # heavily guided by https://www.youtube.com/watch?v=pmDdUT2Txbs
 
+# function to return corresponding 0 if True, 1 if False
+def i(bool):
+    return 0 if bool else 1
+
+def get_current_time():
+    return time.strftime('%H:%M')
+
+# funtion to get remaining time left
+def get_time_left(start, length):
+    return start + length - int(time.time())
+
 def main_window():
 
-    work_period = 1500 # 25 minutes in seconds  
-    time_left = 1500
+    work_period = 1500 # 25 minutes in seconds, by default
+    break_period = 300 # 5 minutes in seconds, by default
+    ''' smaller values, for testing
+    work_period = 2
+    break_period = 2
+    '''
+    periods = [work_period, break_period]
+    work_time = True # True = periods[0], False = periods[1]
+
+    time_left = work_period
     paused = True
 
     title = "Mousekeeping"
     layout = [
-        [psg.T("Welcome to Mousekeeping!")],
+        [psg.T(f"Current time: {get_current_time()}", key="current_time")],
         [psg.T("")],
-        [psg.T("Work Timer:")],
-        [psg.T(f"{(work_period // 60):0>2}:{(work_period % 60):0>2}, {paused}", key='timer')],
+        [psg.T(f"{'Work' if work_time else 'Break'} Timer:", key='timer_name')],
+        [psg.T(f"{(periods[i(work_time)] // 60):0>2}:{(periods[i(work_time)] % 60):0>2}{', Paused' if paused else ''}", key='timer')],
         [psg.B("Start Timer", key='toggle', enable_events=True), psg.B("Reset Timer"), psg.Cancel()]
     ]
 
@@ -39,7 +58,8 @@ def main_window():
         # reset timer event
         elif event == 'Reset Timer' and not paused:
             start_time = int(time.time())
-            time_left = start_time + work_period - int(time.time())
+            time_left = get_time_left(start_time, periods[i(work_time)])
+            work_time = True
             # window.refresh()
             # unsure if reset button should also pause the timer 
 
@@ -49,20 +69,33 @@ def main_window():
             window['toggle'].update("Restart Timer")
             window.refresh()
 
-        if not paused: # update timer if not paused
-            time_left = start_time + work_period - int(time.time())
+        # update timer if not paused
+        if not paused: 
+            time_left = get_time_left(start_time, periods[i(work_time)])
 
-        window['timer'].update(f"{(time_left // 60):0>2}:{(time_left % 60):0>2}, {paused}")
+            # switch phases from work -> break or break -> work
+            if time_left <= 0:  
+                work_time = not work_time
+                paused = True
+                window['toggle'].update("Start Timer")
+                start_time = int(time.time())
+                time_left = get_time_left(start_time, periods[i(work_time)])
+
+        window['timer'].update(f"{(time_left // 60):0>2}:{(time_left % 60):0>2}{', Paused' if paused else ''}")
+        window['current_time'].update(f"Current time: {get_current_time()}")
+        window['timer_name'].update(f"{'Work' if work_time else 'Break'} Timer:")
 
     window.close()
 
 if __name__ == "__main__":  # main code, runs on load
+
     # get path for settings ini file 
     SETTINGS_PATH = Path.cwd()
     settings = psg.UserSettings(
         path=SETTINGS_PATH, filename='config.ini', use_config_file=True, convert_bools_and_none=True
     )
 
+    # fetch ini file settings
     theme = settings['GUI']['theme']
     font_family = settings['GUI']['font_family']
     font_size = settings['GUI']['font_size']
